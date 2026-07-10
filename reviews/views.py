370d6fcf253e1425f0573f828f934dc1cbfeeb2f
osedunme quincy <quincyosedunme@gmail.com>
2026-07-10 16:28:21 +0100

    Add ownership permission so users can only edit/delete their own reviews

--- reviews/views.py
+++ reviews/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Review, Genre
 from .serializers import ReviewSerializer, GenreSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all().order_by('-created_at')
     serializer_class = ReviewSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
