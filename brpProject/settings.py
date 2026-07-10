3b2ab210bd66454cb0cd48deec740f59b472e21c
osedunme quincy <quincyosedunme@gmail.com>
2026-07-10 20:45:02 +0100

    Add search filtering and pagination to reviews

--- brpProject/settings.py
+++ brpProject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'reviews',
     'drf_spectacular',
+    'django_filters',
 ]
 
 
@@ -58,6 +59,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Review, Genre
 from .serializers import ReviewSerializer, GenreSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -9,6 +9,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all().order_by('-created_at')
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['book_title', 'author', 'genre__name']
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
